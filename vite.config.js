f0d740b0506e9d3505834005b77c465e4bbf1ea1
Yuliia Shevchenko <shevichka@icloud.com>
2025-09-09 21:59:27 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/portfolio-shevchenko/', // <-- заміни на свій repo-name/
   plugins: [react()],
 });
