83860302bbe395c41848bbfaf082e7ee91b8cd72
Yuliia Shevchenko <shevichka@icloud.com>
2025-09-10 16:07:16 +0200

    copy updated

--- src/pages/projects/CZ.jsx
+++ src/pages/projects/CZ.jsx
@@ -10,7 +10,7 @@ const CZ = () => {
       request="Creating the Collector's Hub platform for conducting firearm auctions by Colt CZ Group, as well as establishing a marketplace for purchasing and selling limited rights. Functionality includes registration, auction browsing, participation, purchasing rights, and managing authenticity certificates. The design should be attractive and user-friendly. The platform should be developed using modern technologies while ensuring data security."
       solution="The design proposal entailed creating an intuitive and visually appealing interface for the Collector's Hub platform, reflecting the exclusivity of firearm auctions. A strong brand identity was developed to align with Colt CZ Group's values and establish trust among users. The focus was on seamless navigation and clear user pathways for registration, auction browsing, and purchase management to enhance the overall user experience. Furthermore, efforts were made to ensure the platform's responsiveness across various devices, alongside creating engaging visual elements like banners and icons. Collaborative work on security measures and the integration of feedback mechanisms aimed to enhance functionality and user satisfaction."
       text="View website"
-      link="https://hub.coltczgroup.onblocktrust.com/"
+      link=""
       image1={cz1}
       image2={cz2}
     />
